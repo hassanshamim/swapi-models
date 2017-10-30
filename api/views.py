1da5d0f5b92f509786ebf1ba7f5b9feb41fa226f
Hassan Shamim <hassan.has.you@gmail.com>
2017-10-30 13:37:32 -0700

    change Planet Views to use serializer

--- api/views.py
+++ api/views.py
@@ -16,7 +16,7 @@ def planet_list(request):
         planets = Planet.objects.all()[:5]
 
         serializer = PlanetSerializer(planets, many=True)
-        return JsonResponse(serializer.data)
+        return JsonResponse(serializer.data, safe=False)
     else:
         # convert request data from json to a dictionary and create a planet from it
         data = json.loads(request.body)
@@ -33,28 +33,18 @@ def planet_detail(request, id):
     planet = get_object_or_404(Planet, pk=id)
 
     if request.method == 'GET':
-        desired_keys = ['id', 'name', 'population']
-        planet_data = {key: getattr(planet, key) for key in desired_keys}
-        planet_json = json.dumps(planet_data)
-        return HttpResponse(planet_json, content_type='application/json')
+        serializer = PlanetSerializer(planet)
+        return JsonResponse(serializer.data)
 
     elif request.method == 'PUT':
         data = json.loads(request.body)
         # update our planet object if those keys are present in the request
-        planet.name = data.get('name', planet.name)
-        planet.population = data.get('population', planet.population)
-        try:
-            # validate planet data
-            planet.full_clean()
-        except ValidationError as e:
-            errors = json.dumps(e.message_dict)
-            # Return validation errors as json response
-            return HttpResponse(errors, content_type='application/json', status=400)
-        planet.save()
-        desired_keys = ['id', 'name', 'population']
-        planet_data = {key: getattr(planet, key) for key in desired_keys}
-        planet_json = json.dumps(planet_data)
-        return HttpResponse(planet_json, content_type='application/json')
+        serializer = PlanetSerializer(planet, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        else:
+            return JsonResponse(serializer.errors, status=400)
 
     elif request.method == 'DELETE':
         planet.delete()
